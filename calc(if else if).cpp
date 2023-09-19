ef0192f531e53535dabc880c59adec95f69a25e4
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2023-09-19 14:41:40 +0300

    create calc(if else if)

---
+++ calc(if else if).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a;
+    cin >> a;
+    if (a == 0) {
+        cout << "0";
+    }
+    else if (a > 0) {
+        a = 89;
+    }
+    else {
+        a = 98;
+    }
+    
+
+}
+

0e8cdecd708a6a18c151913d047e6bc27f00672c
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2023-09-26 12:51:26 +0300

    create calc(switch case)

---
+++ calc(switch case).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    double num1, num2;
+    char op;
+    cout << "input numbers: ";
+    cin >> num1 >> num2;
+    cout << "input operation: ";
+    cin >> op;
+    switch (op) {
+    case '+':
+        cout << num1 << "+" << num2 << "=" << num1 + num2;
+        break;
+    case '-':
+        cout << num1 << "-" << num2 << "=" << num1 - num2;
+        break;
+    case '*':
+        cout << num1 << "*" << num2 << "=" << num1 * num2;
+        break;
+    case '/':
+        if (num2 == 0) cout << "division by zero";
+        else
+            cout << num1 << "/" << num2 << "=" << num1 / num2;
+        break;
+    default:
+        cout << "unnable operation";
+    }
+
+
+}
+
